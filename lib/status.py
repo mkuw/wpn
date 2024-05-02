715b78978b60dc38ff3a6fd7e218d2d8831d9985
mkuw <mkuw00@gmail.com>
2024-05-02 09:56:18 +0200

    Removed custom view.

--- lib/status.py
+++ lib/status.py
@@ -114,19 +114,13 @@ def status():
 
     fig = make_subplots(rows=1, cols=1)
     multipliers = np.array(multipliers)
-    maxes = []
-    mins = []
     for username, data in points_by_user.items():
         data = np.array(data)
         data -= 12*multipliers
         data = np.cumsum(data)
         fig.add_trace(go.Scatter(x=season_days, y=data, mode="lines+markers", name=username))
-        maxes.append(np.nanmax(data))
-        mins.append(np.nanmin(data))
-    ymax = 10.0
-    ymin = 1.1*np.min(mins)
     fig.update_layout(title_text='Montagne russe WPN', xaxis_title='Data',
-        yaxis_title='Punti', yaxis_range=[ymin, ymax])
+        yaxis_title='Punti')
 
     plot_html = fig.to_html(full_html=False)
 
